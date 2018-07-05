84f87ac40e5c4451d1c3362bad54b3caa6ce5e22
codeachun <40749559+codeachun@users.noreply.github.com>
2018-07-05 09:00:32 +0800

    路由模型綁定

--- app/Http/Controllers/ExamController.php
+++ app/Http/Controllers/ExamController.php
@@ -55,9 +55,9 @@ class ExamController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Exam $exam)
     {
-        //
+        return view('exam.show', ['exam' => $exam]);
     }
 
     /**
