d1643b8c1dd0b4ce7ca14da52b60dc14ffa6aecd
codeachun <40749559+codeachun@users.noreply.github.com>
2018-07-05 16:05:39 +0800

    設定考試與測驗、考生的關聯

--- app/Exam.php
+++ app/Exam.php
@@ -9,9 +9,17 @@ class Exam extends Model
     protected $fillable = [
         'title', 'user_id', 'enable',
     ];
+    protected $casts = [
+        'enable' => 'boolean',
+    ];
 
     public function topics()
     {
         return $this->hasMany('App\Topic');
+    }
+
+    public function tests()
+    {
+        return $this->hasMany('App\Test');
     }
 }
